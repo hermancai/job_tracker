71ab0a394017cea48f19a2e81d296e09ab3a9248
Herman Cai <hermancai3@gmail.com>
2022-02-09 18:29:49 -0800

    Include static path to build

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-}
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => {
   res.json({ status: 200 });
